e6b58aefd70054ba7e56b60e37ebf7351333378e
Sebastian <sgperez@gmail.com>
2016-05-22 00:28:48 +0000

    Initial commit

---
+++ index.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def render_index(param = None):
+	return render_template('main.html', param = param)
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0')
