3e375462f2f0dd269dfd039229327fc4d71c923d
SebMac <sgperez@gmail.com>
2016-05-22 20:14:31 +1000

    github ready

---
+++ test_sf.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+SpeedFlatting Tests
+~~~~~~~~~~~~
+Tests SF application.
+:copyright: (c) 2016 by Sebastian
+"""
+
+# import pytest
+import unittest
+
+class SFTestCase(unittest.TestCase):
+
+    def test_one(self):
+        assert True
+
+
+
+if __name__ == '__main__':
+    unittest.main()
